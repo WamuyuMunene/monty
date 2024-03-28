ae72839390130bae7fa5482eee6639477d615d88
WamuyuMunene <wamuyumunene98@gmail.com>
2024-03-28 06:55:14 -0700

    swap elements

---
+++ monty_swap.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+/**
+ * monty_swap - swaps two top elements
+ *
+ * @stack: pointer
+ * @line_number: int
+ */
+
+void monty_swap(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp;
+
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
+	{
+		set_op_tok_error(short_stack_error(line_number, "swap"));
+		return;
+	}
+
+	tmp = (*stack)->next->next;
+	(*stack)->next->next = tmp->next;
+	(*stack)->next->prev = tmp;
+	if (tmp->next)
+		tmp->next->prev = (*stack)->next;
+	tmp->next = (*stack)->next;
+	tmp->prev = *stack;
+	(*stack)->next = tmp;
+}
