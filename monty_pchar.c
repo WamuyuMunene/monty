7acd292d7ca0253612c3b3940f084b48b8d18f61
WamuyuMunene <wamuyumunene98@gmail.com>
2024-03-28 07:13:07 -0700

    print char

---
+++ monty_pchar.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+/**
+ * monty_pchar - print char in top element
+ *
+ * @stack: pointer
+ * @line_number: int
+ */
+
+void monty_pchar(stack_t **stack, unsigned int line_number)
+{
+	if ((*stack)->next == NULL)
+	{
+		set_op_tok_error(pchar_error(line_number, "stack empty"));
+		return;
+	}
+	if ((*stack)->next->n < 0 || (*stack)->next->n > 127)
+	{
+		set_op_tok_error(pchar_error(line_number,
+					     "value out of range"));
+		return;
+	}
+
+	printf("%c\n", (*stack)->next->n);
+}
