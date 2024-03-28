213e9d9491a15a8aef677f835022a86e4e118cc5
WamuyuMunene <wamuyumunene98@gmail.com>
2024-03-28 07:04:55 -0700

    find modulo of elements

---
+++ monty_mod.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+/**
+ * monty_mod - find modulo of stack element
+ *
+ * @stack: pointer
+ * @line_number: int
+ */
+
+void monty_mod(stack_t **stack, unsigned int line_number)
+{
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
+	{
+		set_op_tok_error(short_stack_error(line_number, "mod"));
+		return;
+	}
+
+	if ((*stack)->next->n == 0)
+	{
+		set_op_tok_error(div_error(line_number));
+		return;
+	}
+
+	(*stack)->next->next->n %= (*stack)->next->n;
+	monty_pop(stack, line_number);
+}
