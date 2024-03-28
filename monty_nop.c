56d175275f7dbb4c4b518191d9a38e6a07b5309b
WamuyuMunene <wamuyumunene98@gmail.com>
2024-03-28 07:11:34 -0700

    does nothing

---
+++ monty_nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * monty_nop - does nothing
+ *
+ * @stack: pointer
+ * @line_number: int
+ */
+
+void monty_nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
