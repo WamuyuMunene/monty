ead08e23dc217587a6f51fee1cfb989f58499448
WamuyuMunene <wamuyumunene98@gmail.com>
2024-03-28 11:06:11 -0700

    int to str

---
+++ int_str.c
@@ -0,0 +1,102 @@
+#include <stdlib.h>
+
+char *get_int(int num);
+unsigned int _abs(int);
+int get_numbase_len(unsigned int num, unsigned int base);
+void fill_numbase_buff(unsigned int num, unsigned int base,
+		       char *buff, int buff_size);
+
+/**
+ * get_int - char to str with int
+ *
+ * @num: int
+ *
+ * Return: char
+ */
+
+char *get_int(int num)
+{
+	unsigned int temp;
+	int length = 0;
+	long num_l = 0;
+	char *ret;
+
+	temp = _abs(num);
+	length = get_numbase_len(temp, 10);
+
+	if (num < 0 || num_l < 0)
+		length++;
+	ret = malloc(length + 1);
+	if (!ret)
+		return (NULL);
+
+	fill_numbase_buff(temp, 10, ret, length);
+	if (num < 0 || num_l < 0)
+		ret[0] = '-';
+
+	return (ret);
+}
+
+/**_abs - abs of int
+ *
+ * @i: int
+ *
+ * Return: int
+ */
+
+unsigned int _abs(int i)
+{
+	if (i < 0)
+		return (-(unsigned int)i);
+	return ((unsigned int)i);
+}
+
+/**
+ * get_numbase_len - len of buffer for ui
+ *
+ * @num: int
+ * @base: int
+ *
+ * Return: int
+ */
+
+int get_numbase_len(unsigned int num, unsigned int base)
+{
+	int len = 1;
+
+	while (num > base - 1)
+	{
+		len++;
+		num /= base;
+	}
+	return (len);
+}
+
+/**
+ * fill_numbers_buff - fills buffer with right int
+ *
+ * @num: int
+ * @base: int
+ * @buff: pointer
+ * @buff_size: int
+ *
+ * Return: void
+ */
+
+void fill_numbase_buff(unsigned int num, unsigned int base,
+			char *buff, int buff_size)
+{
+	int rem, i = buff_size - 1;
+
+	buff[buff_size] = '\0';
+	while (i >= 0)
+	{
+		rem = num % base;
+		if (rem > 9)
+			buff[i] = rem + 87;
+		else
+			buff[i] = rem + '0';
+		num /= base;
+		i--;
+	}
+}
