89081f8883ba91b69501f44bcdd6b49e6da73745
WamuyuMunene <wamuyumunene98@gmail.com>
2024-03-28 12:04:29 -0700

    main file

---
+++ main.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+char **op_toks = NULL;
+
+/**
+ * main - entry point
+ *
+ * @argc: argument count
+ * @argv: variables
+ *
+ * Return: exit success or failure
+ */
+
+int main(int argc, char **argv)
+{
+	FILE *script_fd = NULL;
+	int exit_code = EXIT_SUCCESS;
+
+	if (argc != 2)
+		return (usage_error());
+	script_fd = fopen(argv[1], "r");
+	if (script_fd == NULL)
+		return (f_open_error(argv[1]));
+	exit_code = run_monty(script_fd);
+	fclose(script_fd);
+	return (exit_code);
+}
