4f0642320bc9c4422a80b821690a57fefeb0281a
WamuyuMunene <wamuyumunene98@gmail.com>
2024-03-28 07:28:55 -0700

    rotate elements

---
+++ monty_rotr.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+ * monty_rotr - rotate elements in stack
+ *
+ * @stack: pointer
+ * @line_number: int
+ */
+
+void monty_rotr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *top, *bottom;
+
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
+		return;
+
+	top = (*stack)->next;
+	bottom = (*stack)->next;
+	while (bottom->next != NULL)
+		bottom = bottom->next;
+
+	bottom->prev->next = NULL;
+	(*stack)->next = bottom;
+	bottom->prev = *stack;
+	bottom->next = top;
+	top->prev = bottom;
+
+	(void)line_number;
+}
